4877cf53a6afd99d015a74d68a5bb4ff6457c69b
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-09-03 13:21:07 +0900

    코드 수정

--- nest/Section4/Step9/src/auth/auth.service.ts
+++ nest/Section4/Step9/src/auth/auth.service.ts
@@ -1,27 +1,27 @@
-import {Injectable, UnauthorizedException} from '@nestjs/common';
-import {CatsRepository} from "../cats/cats.repository";
-import {LoginRequestDto} from "./dto/login.request.dto";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CatsRepository } from 'src/cats/cats.repository';
+import { LoginRequestDto } from './dto/login.request.dto';
 import * as bcrypt from 'bcrypt';
-import {JwtService} from "@nestjs/jwt";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly catsRepository: CatsRepository,
-                private jwtService: JwtService,
-    ) {
-    }
+    constructor(
+        private readonly catsRepository: CatsRepository,
+        private jwtService: JwtService,
+    ) {}
 
     async jwtLogIn(data: LoginRequestDto) {
-        const {email, password} = data;
+        const { email, password } = data;
 
-        // 해당하는 이메일이 있는지 검증
+        //* 해당하는 email이 있는지
         const cat = await this.catsRepository.findCatByEmail(email);
 
         if (!cat) {
             throw new UnauthorizedException('이메일과 비밀번호를 확인해주세요.');
         }
 
-        // 비밀번호가 일치하는지 확인
+        //* password가 일치한지
         const isPasswordValidated: boolean = await bcrypt.compare(
             password,
             cat.password,
@@ -31,13 +31,10 @@ export class AuthService {
             throw new UnauthorizedException('이메일과 비밀번호를 확인해주세요.');
         }
 
-        // 토큰 발급
-        const payload = {email: email, sub: cat.id};
+        const payload = { email: email, sub: cat.id };
 
         return {
-            data: {
-                token: this.jwtService.sign(payload),
-            }
+            token: this.jwtService.sign(payload),
         };
     }
 }
