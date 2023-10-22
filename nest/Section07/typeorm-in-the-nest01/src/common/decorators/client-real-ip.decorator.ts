4bb30a56e74d95cdae624918a5f1839a7c13bc0d
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-10-22 13:24:41 +0900

    보일러플레이트 코드 리뷰 및 테스팅 소개

--- nest/Section07/typeorm-in-the-nest01/src/common/decorators/client-real-ip.decorator.ts
+++ nest/Section07/typeorm-in-the-nest01/src/common/decorators/client-real-ip.decorator.ts
@@ -2,6 +2,7 @@ import * as requestIp from 'request-ip'
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 import { Request } from 'express'
 
+// 현재 사용자의 ip를 추출해줌: 조회수, 방문객 나타낼 때 사용한다고 보면됨.
 export const ClientIp = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest()
