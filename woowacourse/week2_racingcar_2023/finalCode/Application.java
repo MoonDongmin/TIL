406d04f4a7f60e652ba17adc41b1006ae8db87ff
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-10-31 21:21:45 +0900

    코드 수정

---
+++ woowacourse/week2_racingcar_2023/finalCode/Application.java
@@ -0,0 +1,12 @@
+package week2_racingcar_2023.finalCode;
+
+public class Application {
+  public static void main(String[] args) {
+    try {
+      Game game = new Game();
+      game.start();
+    } catch (IllegalArgumentException e) {
+      System.err.println("입력이 잘못되었습니다: " + e.getMessage());
+    }
+  }
+}
---
+++ woowacourse/week2_racingcar_2023/finalCode/Game.java
@@ -0,0 +1,19 @@
+package week2_racingcar_2023.finalCode;
+
+public class Game {
+  public void start() {
+    InputManager inputManager = new InputManager();
+    String[] carNames = inputManager.getCarNames();
+    int numAttempts = inputManager.getNumAttempts();
+
+    Race race = new Race(carNames);
+    OutputManager outputManager = new OutputManager();
+
+    for (int attempt = 0; attempt < numAttempts; attempt++) {
+      race.race();
+      outputManager.displayCarPositions(race.getCars());
+    }
+
+    outputManager.announceWinners(race.getCars());
+  }
+}
