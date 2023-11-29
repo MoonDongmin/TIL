4702ace9affb9c5e08d9b97293f5f043167b8a22
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-11-29 15:35:59 +0900

    연산자

---
+++ java/basic/operation/OperationEx3.java
@@ -0,0 +1,12 @@
+package basic.operation;
+
+public class OperationEx3 {
+  public static void main(String[] args) {
+    int score = 80;
+    if (score >= 80 && score <= 100) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+  }
+}
