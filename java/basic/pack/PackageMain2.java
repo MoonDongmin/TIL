c110e712ed10dc4fae10249a644276d63c161ff1
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-12-14 20:29:59 +0900

    패키지

---
+++ java/basic/pack/PackageMain2.java
@@ -0,0 +1,13 @@
+package basic.pack;
+
+//import basic.pack.a.User;
+//import basic.pack.a.User2;
+
+import basic.pack.a.*;
+
+public class PackageMain2 {
+  public static void main(String[] args) {
+    Data data = new Data();
+    User user = new User();
+  }
+}
---
+++ java/basic/pack/a/User.java
@@ -0,0 +1,10 @@
+package basic.pack.a;
+
+import basic.pack.Data;
+
+public class User {
+  public User(){
+    System.out.println("패키지 pack.a 회원 생성");
+  }
+
+}
