f2bbc042e9c22aa47c53d68bbf3757b6952d49f3
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-06-03 20:32:28 +0900

    폴더 구조 변경 및 migration중...

---
+++ nest/my/capstone-modify/src/main/io/wisoft/capstone/gc/app.module.ts
@@ -0,0 +1,11 @@
+import { AppController } from "@gc/app.controller";
+import { AppService } from "@gc/app.service";
+import { Module } from "@nestjs/common";
+import { RecordsModule } from "./records/records.module";
+
+@Module({
+  imports: [RecordsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
